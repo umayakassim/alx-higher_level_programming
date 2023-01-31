beb79203017fa33ca645fccd30dce1a8aca3ae5c
Ukassim-dev <69954868+umayakassim@users.noreply.github.com>
2023-01-31 09:22:24 +0300

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    """Print the number of and list of arguments."""
     import sys
-
-    count = len(sys.argv) - 1
-    if count == 0:
-        print("0 arguments.")
-    elif count == 1:
-        print("1 argument:")
+    arg_length = len(sys.argv)
+    if arg_length == 1:
+        print("{} arguments.".format(arg_length - 1))
+    elif arg_length == 2:
+        print("{} argument:".format(arg_length - 1))
     else:
-        print("{} arguments:".format(count))
-    for i in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        print("{} arguments:".format(arg_length - 1))
+
+    for i in range(1, arg_length):
+        print("{}: {}".format(i, sys.argv[i]))
         
